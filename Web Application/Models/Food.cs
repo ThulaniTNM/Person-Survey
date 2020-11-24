31c57b1d76b8224d3a246002628809e27f8a09c2
Thulani Mabaso <Thulanitnm@gmail.com>
2020-11-24 21:22:20 +0200

    Person survey Project upload

---
+++ Web Application/Models/Food.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Web_Application.Models {
+    /*
+     Things to be removed.
+        1. FoodDetails property,replace with database , Keys and value has same structure as database table. Use those instead for consistecy.
+        2. FoodDetails property instantiation in constructor.
+     */
+    public class Food {
+        /*Start of database design*/
+        [Key]
+        public int FoodID { get; set; }
+        public string Name { get; set; }
+        public virtual ICollection<PersonFood> PersonFoods { get; set; }
+        /*End of database design*/
+
+        //Structure for generating view.
+
+
+        [NotMapped]
+        public Dictionary<string,string> FoodDetails { get; set; }  // Food structure ,use to generate its value in the view only.
+       
+        [NotMapped]
+        // use for getting key of the selected food user selects.
+        //  use for generating checkbox for each available food that user can select.
+        public Dictionary<string,bool> SelectedFood { get; set; } 
+
+        // Use constructor to set key and default value for selectedFood property before user selects.
+        public Food( ) {
+
+            // This data not important ,can be replaced by db data for constistency.
+            FoodDetails = new Dictionary<string, string>() {
+                {"1","Pizza" },
+                {"2","Pasta" },
+                {"3","Pap and Wors" },
+                {"4","Chicken Stir Fry" },
+                {"5","Beef Stir Fry" },
+                {"6","Other" },
+            };
+
+            // user has not selected food when view is rendered so set value to false for food not selected.
+            // Key  used to keep track of which food item has been selected.
+            // selectedFood item will have value changed to true.
+            // will use its key to obtain information of selected food item(true value) from db
+            SelectedFood = new Dictionary<string, bool>();
+            for (int i = 1; i< 7; i++) {
+                SelectedFood.Add(i.ToString(), false);
+            }
+
+
+
+        }
+    }
+     
+}
---
+++ Web Application/Models/Person.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+//C: \Users\User\Desktop\Programs\Package Apps\mvc\Template no auth\Web Application\
+namespace Web_Application.Models {
+    public class Person {
+        /*Start of database design*/
+        [Key]
+        public int PersonID { get; set; }
+
+        [Required(ErrorMessage = "Surname required")]
+        public string Surname { get; set; }
+
+        [Required(ErrorMessage = "Name required")]
+        [Display(Name = "First Names")]
+        public string FirstNames { get; set; }
+
+        /*Self made expression ^(\+27|0)[\s-]?(\d{9}|(\d{2}-\d{3}-\d{4})|(\d{2}\s\d{3}\s\d{4})|(\d{2}[\s-]\d{7}))$
+                   All possible numbers in south africa.
+
+                    +27760518556
+                    0760518556
+
+                   +2776-051-8556
+                    076-051-8556
+
+                    +27 76 051 8556
+                     076 051 8556
+                    +27 76-051-8556
+                    
+
+                    076 0518556
+                    076-0518556
+                    +27-765339085
+         */
+        [Required(ErrorMessage = "Contact required")]
+        [RegularExpression(@"^(\+27|0)[\s-]?(\d{9}|(\d{2}-\d{3}-\d{4})|(\d{2}\s\d{3}\s\d{4})|(\d{2}[\s-]\d{7}))$", ErrorMessage = "Please enter valid phone no Eg 076305185566")] // Review reqular expression
+        [Display(Name = "Contact Number")]
+        public string Contact { get; set; }
+
+        [Required(ErrorMessage = "Date of birth required")]
+        [DataType(DataType.Date)]
+        [Column(TypeName = "Date")]
+        public DateTime Date { get; set; }
+
+        [Required(ErrorMessage = "Age required")]
+        [Range(5, 120, ErrorMessage = "Age should be between 5 and 120")]
+        public int Age { get; set; }
+        public virtual ICollection<PersonFood> PersonFoods { get; set; }
+        public virtual ICollection<PersonLiking> PersonLikings { get; set; }
+
+        /*End of database design*/
+
+
+        // Food and liking selected collected from the form
+        [NotMapped]
+        public Food Food { get; set; }
+
+        [NotMapped]
+        public Liking Liking { get; set; }
+
+        public Person( ) {
+            Food = new Food();
+            Liking = new Liking();
+        }
+    }
+}
